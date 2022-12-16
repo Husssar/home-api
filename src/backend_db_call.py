4af237680c9d7de15749a73c365e84f6d8e8281f
Tobias Svensson <husssar@gmail.com>
2022-12-16 12:27:09 +0100

    adding consuming to backend

--- src/backend_db_call.py
+++ src/backend_db_call.py
@@ -1,8 +1,7 @@
-import random
 
 import cred
 import pymysql
-import json
+
 
 
 def get_temperature(meter_id, medium_type):
@@ -47,9 +46,72 @@ def get_electricity_price():
         print(e)
 
 def get_electricity_consuming():
-    consuming = random.uniform(300.00, 5000.00)
-    print(f"consuming {consuming}")
-    return round(consuming, 2)
+    qry = []
+
+    try:
+        cnx = pymysql.connect(user=cred.SQLUW, password=cred.USERPW, host=cred.HOST, database=cred.DATABASE)
+
+        qry.append(f'SELECT value, effiency, obis.obis_str FROM grid_data_latest')
+        qry.append(f'inner join obis on obis.obis_id = grid_data_latest.obis_id')
+
+        q = " ".join(qry)
+        print(q)
+        cur = cnx.cursor()
+        cur.execute(q)
+        cnx.commit()
+        response = cur.fetchall()
+
+        l1_effekt = ''
+        l2_effekt = ''
+        l3_effekt = ''
+        total_effekt = ''
+        l1 = 0
+        l2 = 0
+        l3 = 0
+        total = 0
+        l1_value = 0
+        l2_value = 0
+        l3_value = 0
+        total_value = 0
+
+        if response[0][2] == 'L1 Aktiv Effekt Uttag Momentan effekt':
+            l1_value = response[0][0]
+            l1_effekt = response[0][1]
+            l1 = f'{response[0][0]} {response[0][1]}'
+
+        if response[1][2] == 'L2 Aktiv Effekt Uttag Momentan effekt':
+            l2_value = response[1][0]
+            l2_effekt = response[1][1]
+            l2 = f'{response[0][0]} {response[0][1]}'
+
+        if response[2][2] == 'L3 Aktiv Effekt Uttag Momentan effekt':
+            l3_value = response[2][0]
+            l3_effekt = response[2][1]
+            l3 = f'{response[2][0]} {response[2][1]}'
+
+        if response[3][2] == 'Aktiv Effekt Uttag\tMomentan trefaseffekt':
+            total_value = response[3][0]
+            total_effekt = response[3][1]
+            total = f'{response[3][0]} {response[3][1]}'
+
+        _str = {
+            "l1_value": l1_value,
+            "l1_effekt": l1_effekt,
+            "l1": l1,
+            "l2_value": l2_value,
+            "l2_effekt": l2_effekt,
+            "l2": l2,
+            "l3_value": l3_value,
+            "l3_effekt": l3_effekt,
+            "l3": l3,
+            "total_value": total_value,
+            "total_effekt": total_effekt,
+            "total_out": total,
+               }
+        print(_str)
+        return _str
+    except Exception as e:
+        print(e)
 
 def get_electricity_added_power():
     qry = []
